dbb26ff03fd6d6bb8ee46a6162b2ba2ba3836fd3
Dawid Dylus <dylusdawid@gmail.com>
2019-03-31 20:58:31 +0200

    Adding new Actor Bomb. Setting default mesh.

---
+++ Bomberman/Source/Bomberman/Private/Bomb.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Bomb.h"
+#include "ConstructorHelpers.h"
+#include "Components/StaticMeshComponent.h"
+
+// Sets default values
+ABomb::ABomb()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	SuperMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("SuperMesh"));
+	//SuperMesh->SetupAttachment(RootComponent);
+	static ConstructorHelpers::FObjectFinder<UStaticMesh>MyMesh(TEXT("/Game/StarterContent/Shapes/Shape_Sphere.Shape_Sphere"));
+	SuperMesh->SetStaticMesh(MyMesh.Object);
+
+	
+}
+
+// Called when the game starts or when spawned
+void ABomb::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABomb::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
